5048e2d43c2fdeb2867de0ce0fde6207af6f8d88
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-05-07 22:13:12 +0545

    used grid instead of flex, added shadows and pop off effect in charts

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -130,106 +130,115 @@ function AdminDashboard() {
   const [auth] = useAuthContext();
   return (
     <>
-      <section className="container  flex justify-start items-start gap-10 ">
+      <section className="  flex justify-start items-start  ">
         <AdminMenu />
-        <div className="mt-10">
-          <div className="flex gap-2">
-            <span>Admin: </span>
-            <span className="col-span-2 text-secondary font-bold">
-              {auth.user.name}
-            </span>
-          </div>
-          <div className="flex gap-2">
-            <span>Email: </span>
-            <span className="col-span-2 text-secondary font-bold">
-              {auth.user.email}
-            </span>
-          </div>
-          <h1 className="font-head font-extrabold mt-10 mb-5 text-2xl">
-            Stats
-          </h1>
-          <div className="flex flex-wrap gap-10">
-            <div
-              className="flex gap-4 justify-center items-center shadow-xl   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
-              data-aos="fade-up"
-            >
-              <i className="fa-solid fa-boxes-stacked h-14 w-14 flex justify-center items-center text-2xl bg-secondaryTint text-secondary rounded-full p-3"></i>
-              <div>
-                <span className="font-head font-medium text-sm">
-                  Total Products
-                </span>
-                <div className="text-xl font-bold ">{products.length}</div>
-              </div>
+        <div className="mt-10 mx-auto">
+          <div className="max-w-[120rem] mx-auto">
+            <div className="flex gap-2">
+              <span>Admin: </span>
+              <span className="col-span-2 text-secondary font-bold">
+                {auth.user.name}
+              </span>
             </div>
-
-            <div
-              className="flex gap-4 justify-center items-center shadow-xl   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
-              data-aos="fade-up"
-            >
-              <i className="fa-solid fa-hourglass-start h-14 w-14 flex items-center justify-center text-2xl bg-[#dedede] text-[#9d9d9d] rounded-full p-3"></i>
-              <div>
-                <span className="font-head font-medium text-sm">
-                  Orders to deliver
-                </span>
-                <div className="text-xl font-bold ">{paidOrders.length}</div>
-              </div>
+            <div className="flex gap-2">
+              <span>Email: </span>
+              <span className="col-span-2 text-secondary font-bold">
+                {auth.user.email}
+              </span>
             </div>
-
-            <div
-              className="flex gap-4 justify-center items-center shadow-xl   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
-              data-aos="fade-up"
-            >
-              <i className="fa-solid fa-truck h-14 w-14 flex justify-center items-center text-2xl bg-green-300 text-green-700 rounded-full p-3"></i>
-              <div>
-                <span className="font-head font-medium text-sm">
-                  Delivered Orders
-                </span>
-                <div className="text-xl font-bold ">
-                  {deliveredOrders.length}
+            <h1 className="font-head font-extrabold mt-10 mb-5 text-2xl">
+              Stats
+            </h1>
+            <div className="grid grid-cols-4 gap-10">
+              <div
+                className="flex gap-4 justify-center items-center shadow-md   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
+                data-aos="fade-up"
+              >
+                <i className="fa-solid fa-boxes-stacked h-14 w-14 flex justify-center items-center text-2xl bg-secondaryTint text-secondary rounded-full p-3"></i>
+                <div>
+                  <span className="font-head font-medium text-sm">
+                    Total Products
+                  </span>
+                  <div className="text-xl font-bold ">{products.length}</div>
                 </div>
               </div>
-            </div>
 
-            <div
-              className="flex gap-4 justify-center items-center shadow-xl   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
-              data-aos="fade-up"
-            >
-              <i className="fa-solid fa-users h-14 w-14 flex justify-center items-center text-2xl bg-blue-300 text-blue-900 rounded-full p-3"></i>
-              <div>
-                <span className="font-head font-medium text-sm">
-                  Total Users
-                </span>
-                <div className="text-xl font-bold ">{users.length}</div>
+              <div
+                className="flex gap-4 justify-center items-center shadow-md   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
+                data-aos="fade-up"
+              >
+                <i className="fa-solid fa-hourglass-start h-14 w-14 flex items-center justify-center text-2xl bg-[#dedede] text-[#9d9d9d] rounded-full p-3"></i>
+                <div>
+                  <span className="font-head font-medium text-sm">
+                    Orders to deliver
+                  </span>
+                  <div className="text-xl font-bold ">{paidOrders.length}</div>
+                </div>
               </div>
-            </div>
 
-            <div
-              className="flex gap-4 justify-center items-center shadow-xl   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
-              data-aos="fade-up"
-            >
-              <i className="fa-solid fa-indian-rupee-sign h-14 w-14 flex justify-center items-center text-2xl bg-[#F9E486] text-[#E1A95F] rounded-full p-3"></i>
-              <div>
-                <span className="font-head font-medium text-sm">
-                  Total Revenues
-                </span>
-                <div className="text-xl font-bold ">Rs.{totalRevenue}</div>
+              <div
+                className="flex gap-4 justify-center items-center shadow-md   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
+                data-aos="fade-up"
+              >
+                <i className="fa-solid fa-truck h-14 w-14 flex justify-center items-center text-2xl bg-green-300 text-green-700 rounded-full p-3"></i>
+                <div>
+                  <span className="font-head font-medium text-sm">
+                    Delivered Orders
+                  </span>
+                  <div className="text-xl font-bold ">
+                    {deliveredOrders.length}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
 
-          <div className="flex justify-between gap-4 mt-10">
-            <div className="my-10 w-[500px]" data-aos="fade-up">
-              <h1 className="font-head font-extrabold mb-5 text-2xl">
-                Revenue Over Time
-              </h1>
-              <Line data={chartData} className="" />
-            </div>
+              <div
+                className="flex gap-4 justify-center items-center shadow-md   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
+                data-aos="fade-up"
+              >
+                <i className="fa-solid fa-users h-14 w-14 flex justify-center items-center text-2xl bg-blue-300 text-blue-900 rounded-full p-3"></i>
+                <div>
+                  <span className="font-head font-medium text-sm">
+                    Total Users
+                  </span>
+                  <div className="text-xl font-bold ">{users.length}</div>
+                </div>
+              </div>
 
-            <div className="my-10 w-[500px]" data-aos="fade-up">
-              <h1 className="font-head font-extrabold mb-5 text-2xl">
-                Top Selling Products
-              </h1>
-              <Bar data={topSellingProductsChartData} />
+              <div
+                className="flex gap-4 justify-center items-center shadow-md   relative px-5 py-2  text-white transition-all ease-in duration-100 border"
+                data-aos="fade-up"
+              >
+                <i className="fa-solid fa-indian-rupee-sign h-14 w-14 flex justify-center items-center text-2xl bg-[#F9E486] text-[#E1A95F] rounded-full p-3"></i>
+                <div>
+                  <span className="font-head font-medium text-sm">
+                    Total Revenues
+                  </span>
+                  <div className="text-xl font-bold ">Rs.{totalRevenue}</div>
+                </div>
+              </div>
+              <div></div>
+              <div></div>
+              <div></div>
+
+              <div
+                className="my-10 w-[500px] shadow-md p-3 border col-span-2"
+                data-aos="fade-up"
+              >
+                <h1 className="font-head font-extrabold mb-5 text-2xl">
+                  Revenue Over Time
+                </h1>
+                <Line data={chartData} className="" />
+              </div>
+
+              <div
+                className="my-10 w-[500px] shadow-md p-3 border col-span-2"
+                data-aos="fade-up"
+              >
+                <h1 className="font-head font-extrabold mb-5 text-2xl">
+                  Top Selling Products
+                </h1>
+                <Bar data={topSellingProductsChartData} />
+              </div>
             </div>
           </div>
         </div>
