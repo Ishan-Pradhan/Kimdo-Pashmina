ae90765810afcf968f54c19b26aa490f7b93561c
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2024-05-09 09:08:02 +0545

    added disabled curser when pagination is disabled

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -21,18 +21,18 @@ function Pagination({ currentPage, itemsPerPage, totalItems, onPageChange }) {
   return (
     <div className="flex justify-end gap-4 items-center mt-20 text-secondary">
       <button
-        className="text-secondary flex items-center gap-2 cursor-pointer "
+        className="text-secondary flex items-center gap-2  "
         onClick={handlePrevClick}
         disabled={currentPage === 1}
       >
         <i
           className={`fa-solid fa-chevron-left text-secondary text-md ${
-            currentPage === 1 ? "text-secondaryTint" : ""
+            currentPage === 1 ? "text-secondaryTint cursor-not-allowed" : ""
           }`}
         ></i>
         <span
           className={`text-secondary ${
-            currentPage === 1 ? "text-secondaryTint" : ""
+            currentPage === 1 ? "text-secondaryTint cursor-not-allowed" : ""
           }`}
         >
           Prev
@@ -40,20 +40,24 @@ function Pagination({ currentPage, itemsPerPage, totalItems, onPageChange }) {
       </button>
       <span>{`Page ${currentPage} of ${totalPages}`}</span>
       <button
-        className="text-secondary flex items-center gap-2 cursor-pointer "
+        className="text-secondary flex items-center gap-2  "
         onClick={handleNextClick}
         disabled={currentPage === totalPages}
       >
         <span
           className={`text-secondary ${
-            currentPage === totalPages ? "text-secondaryTint" : ""
+            currentPage === totalPages
+              ? "text-secondaryTint cursor-not-allowed"
+              : ""
           }`}
         >
           Next
         </span>{" "}
         <i
           className={`fa-solid fa-chevron-right text-secondary text-md ${
-            currentPage === totalPages ? "text-secondaryTint" : ""
+            currentPage === totalPages
+              ? "text-secondaryTint cursor-not-allowed"
+              : ""
           }`}
         ></i>
       </button>
