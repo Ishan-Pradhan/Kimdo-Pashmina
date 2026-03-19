2d15a648e910aeb8af8695cfe3ac562c108e4151
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-03-19 15:39:07 +0545

    fix: fix case of AboutUs when import

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Features from "../components/Homepage/Features";
 import PageNavigation from "../components/PageNavigation";
-import TestimonialSection from "../components/Homepage/testimonialSection";
+import TestimonialSection from "../components/Homepage/TestimonialSection";
 
 function AboutUs() {
   return (
